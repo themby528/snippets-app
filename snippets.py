c574fe134f04f94fca9c4267af0c93057b6e23d5
themby528 <taylor.arrowood@gmail.com>
2015-03-01 20:52:56 +0000

    Initial commit

---
+++ snippets.py
@@ -0,0 +1,22 @@
+import logging
+#set the log output file, and the log level
+logging.basicConfig(filename="snippets.log",level=logging.DEBUG)
+
+#Stubs - a function that is defoned but does nothing useful
+def put(name, snippet):
+    """
+    Store a snippet with an associated name.
+    Returns the name and snippet
+    """
+    logging.error("FIXME: Unimplemented - put ({!r}, {!r})".format(name,sinppet))
+    return name, snippet
+
+def get(name):
+    """
+    Retrieve the snippet with a given name. 
+    If there is no such snippet provide error message and ask user if they would like to create the snippet
+    Returns the snippet.
+    """
+    logging.erro("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+    
